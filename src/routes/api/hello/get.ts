3bacc2d3ed18d7db07df0dd5220a78e254abe5a9
skitsanos <info@skitsanos.com>
2023-04-28 12:03:32 +0300

    chore: support for code coverage reporting

--- src/routes/api/hello/get.ts
+++ src/routes/api/hello/get.ts
@@ -1,18 +1,20 @@
 import {FastifyReply, FastifyRequest} from 'fastify';
 import {FatsyDynamicRoute, FatsyRouteUtils} from '@/utils/loader';
+import apiResponse from '@/utils/apiResponse';
 
 const get: FatsyDynamicRoute = {
-    handler: (_request: FastifyRequest, response: FastifyReply, utils:FatsyRouteUtils) =>
+    handler: (_request: FastifyRequest, response: FastifyReply, utils: FatsyRouteUtils) =>
     {
         const {
             log,
             config
         } = utils;
+
         const {config: appConfig} = config;
 
         log.info('Got hello handler');
 
-        response.send({
+        apiResponse(response, 200, {
             debug: process.env['NODE_ENV'] !== 'production',
             appConfig
         });
